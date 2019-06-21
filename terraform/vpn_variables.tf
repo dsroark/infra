38d0b455c95187578fb17d3610351e491d7bea59
Devin Roark <roarkd@gmail.com>
2019-06-20 21:49:04 -0400

    set variable defaults

--- terraform/vpn_variables.tf
+++ terraform/vpn_variables.tf
@@ -1,10 +1,17 @@
-variable "aws_region" {}
-variable "aws_profile" {}
+variable "aws_region" {
+    default = "us-east-2"
+}
+variable "aws_profile" {
+    default = "default"
+}
 data "aws_availability_zones" "available" {}
-variable "vpc_cidr" {}
+variable "vpc_cidr" {
+    default = "10.1.0.0/16"
+}
 variable "localip" {}
 variable "pubkey" {}
 
 variable "cidrs" {
     type = "map"
+    default = '{ "public1": "10.1.0.0/24"}/
 }
